1ba8f9b042b3574ebfaf70c1dca5977ebfd76066
Yazeed Mohamed <yazeedmo0810@gmail.com>
2023-07-16 00:55:30 +0200

    Added InsertionSort.java

--- Java/DataStructures/SinglyLinkedList.java
+++ Java/DataStructures/SinglyLinkedList.java
@@ -1,11 +1,11 @@
-package org.example;
+package org.dsahub.datastructures;
 
 public class SinglyLinkedList<E> {
 
     //-------------- nested Node class ---------------
     private static class Node<E> {
-        private final E element;            // Reference to the element stores at this Node
-        private Node<E> next;               // Reference to the subsequent Node in the list
+        private final E element;                // Reference to the element stores at this Node
+        private Node<E> next;                   // Reference to the subsequent Node in the list
         public Node(E e, Node<E> n) {
             element = e;
             next = n;
@@ -18,16 +18,16 @@ public class SinglyLinkedList<E> {
 
 
     // Instance variables of the SinglyLinkedList
-    private Node<E> head = null;            // Head node of the list (or null if empty)
-    private Node<E> tail = null;            // Last node of the list (or null if empty)
-    private int size = 0;                   // Number of nodes in the list
+    private Node<E> head = null;                // Head node of the list (or null if empty)
+    private Node<E> tail = null;                // Last node of the list (or null if empty)
+    private int size = 0;                       // Number of nodes in the list
 
-    public SinglyLinkedList() { }           // Constructs and initially empty list
+    public SinglyLinkedList() { }               // Constructs and initially empty list
 
     // Access methods
     public int size() { return size; }
     public boolean isEmpty() { return size == 0; }
-    public E first() {                      // Returns (but does not remove) the first element
+    public E first() {                          // Returns (but does not remove) the first element
         if (isEmpty()) { return null; }
         return head.getElement();
     }
@@ -37,38 +37,37 @@ public class SinglyLinkedList<E> {
     }
 
     // Update methods
-    public void addFirst(E e) {             // Adds element e to the front of te list
-        head = new Node<>(e, head);         // Create and link a new Node
+    public void addFirst(E e) {                 // Adds element e to the front of te list
+        head = new Node<>(e, head);             // Create and link a new Node
         if (size == 0) {
-            tail = head;                    // Special case: new node becomes tail also
+            tail = head;                        // Special case: new node becomes tail also
         }
         size++;
     }
 
-    public void addLast(E e) {              // Adds element e to the end of the list
+    public void addLast(E e) {                  // Adds element e to the end of the list
         Node<E> newest = new Node<>(e, null);   // Node will eventually be the tail
         if (isEmpty()) {
-            head = newest;                  // Special case: previously empty list
+            head = newest;                      // Special case: previously empty list
         }
         else {
-            tail.setNext(newest);           // New node after existing tail
+            tail.setNext(newest);               // New node after existing tail
         }
-        tail = newest;                      // New node becomes the tail
+        tail = newest;                          // New node becomes the tail
         size++;
     }
 
-    public E removeFirst() {                // Removes and returns the firs element
-        if (isEmpty()) {                    // Nothing more to remove
+    public E removeFirst() {                    // Removes and returns the firs element
+        if (isEmpty()) {                        // Nothing more to remove
             return null;
         }
         E answer = head.getElement();
-        head = head.getNext();              // Will become null if list had only one Node
+        head = head.getNext();                  // Will become null if list had only one Node
         size--;
         if (size == 0) {
-            tail = null;                    // Special case as list is now empty
+            tail = null;                        // Special case as list is now empty
         }
         return answer;
     }
-
 
 }
